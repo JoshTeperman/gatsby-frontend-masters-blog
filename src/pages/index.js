90dc9a64e58226069a04706e8d958e27eb949f6d
Josh Teperman <joshteperman@gmail.com>
2019-10-14 22:07:34 +1100

    Add styles and Layout
    - create Layout component
    - create Global styles
    - add layout styles to index and about pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>Home</h1>
     <p>Welcome to my website!</p>
     <Link to="/about">Learn About Me &rarr;</Link>
-  </>
+  </Layout>
 );
