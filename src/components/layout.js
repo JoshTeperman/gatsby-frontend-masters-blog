d13363034ba7fc022256ab9b42820419c347bf5d
Josh Teperman <joshteperman@gmail.com>
2019-10-18 22:13:13 +1100

    Add metadata
    - Configure metadata with helmet
    - Create Hook using graphql query for site metadata
    - Add metadata to Layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,69 +1,80 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
+import Helmet from 'react-helmet';
 import Header from './header';
+import useSiteMetadata from '../hooks/useSiteMetadata';
 
-const Layout = ({ children }) => (
-  <>
-    <Global
-      styles={css`
-        * {
-          margin: 0;
-          box-sizing: border-box;
-        }
-
-        * + * {
-          margin-top: 1rem;
-        }
-
-        html,
-        body {
-          margin: 0;
-          color: #555;
-          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-            Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-          font-size: 18px;
-          line-height: 1.4;
-
-          /* remove margin for the main div that Gatsby mounts into */
-          > div {
-            margin-top: 0;
+const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata();
+  return (
+    <>
+      <Global
+        styles={css`
+          * {
+            margin: 0;
+            box-sizing: border-box;
           }
 
-          h1,
-          h2,
-          h3,
-          h4,
-          h5,
-          h6 {
-            line-height: 1.1;
-            color: #222;
-            /* anything that comes after heading has a slightly smaller margin-top */
-            + * {
-              margin-top: 0.5rem;
+          * + * {
+            margin-top: 1rem;
+          }
+
+          html,
+          body {
+            margin: 0;
+            color: #555;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+              Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+              sans-serif;
+            font-size: 18px;
+            line-height: 1.4;
+
+            /* remove margin for the main div that Gatsby mounts into */
+            > div {
+              margin-top: 0;
+            }
+
+            h1,
+            h2,
+            h3,
+            h4,
+            h5,
+            h6 {
+              line-height: 1.1;
+              color: #222;
+              /* anything that comes after heading has a slightly smaller margin-top */
+              + * {
+                margin-top: 0.5rem;
+              }
+            }
+
+            strong {
+              color: #222;
+            }
+
+            li {
+              margin-top: 0.25rem;
             }
           }
-
-          strong {
-            color: #222;
-          }
-
-          li {
-            margin-top: 0.25rem;
-          }
-        }
-      `}
-    />
-    <Header />
-    <main
-      css={css`
-        margin: 2rem auto 4rem;
-        max-width: 550px;
-        width: 90vw;
-      `}
-    >
-      {children}
-    </main>
-  </>
-);
+        `}
+      />
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      <Header />
+      <main
+        css={css`
+          margin: 2rem auto 4rem;
+          max-width: 550px;
+          width: 90vw;
+        `}
+      >
+        {children}
+      </main>
+    </>
+  );
+};
 
 export default Layout;
