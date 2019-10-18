6e02efc6bcba608953b8c4f2c4def7bbc1df070e
Josh Teperman <joshteperman@gmail.com>
2019-10-18 23:49:15 +1100

    Add post content to post templates from MDX files
    - Create graphql query, filtering by slug
    - Add data to page

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,18 +3,31 @@ import { graphql } from 'gatsby';
 import { css } from '@emotion/core';
 import Layout from '../components/layout';
 import ReadLink from '../components/read-link';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 
-const PostTemplate = () => (
+export const query = graphql`
+  query($slug: String) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        author
+      }
+      body
+    }
+  }
+`;
+
+const PostTemplate = ({ data: { mdx: post } }) => (
   <Layout>
-    <h1>post title</h1>
+    <h1>{post.frontmatter.title}</h1>
     <p
       css={css`
         font-size: 0.75rem;
       `}
     >
-      Posted by post.author
+      Posted by {post.frontmatter.author}
     </p>
-    <p>post.excerpt</p>
+    <MDXRenderer>{post.body}</MDXRenderer>
     <ReadLink to="/">&larr; Back to all posts</ReadLink>
   </Layout>
 );
