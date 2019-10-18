86600c79c42e23b93bfa0e029fc865f5d12637c3
Josh Teperman <joshteperman@gmail.com>
2019-10-18 22:24:07 +1100

    gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,25 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'Gatsby Blog',
+    description: 'Frontend Masters Gatsby Blog tutorial',
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'posts',
+      },
+    },
+  ],
 };
