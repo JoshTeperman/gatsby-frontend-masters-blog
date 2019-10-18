be0163a3e20ba2d125f5909cb8e6bfc0e9e90a93
Josh Teperman <joshteperman@gmail.com>
2019-10-18 22:22:02 +1100

    Add Post preview to index.js
    - Create usePosts hook with graphql query to fetch posts
    - Create PostPreview component
    - Create ReadLink Link component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import usePosts from '../hooks/usePosts';
+import PostPreview from '../components/post-preview';
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>Welcome to my website!</p>
-    <Link to="/about">Learn About Me &rarr;</Link>
-  </Layout>
-);
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Welcome to my website!</p>
+      <Link to="/about">Learn About Me &rarr;</Link>
+      {posts.map(post => (
+        <PostPreview post={post} key={post.slug} />
+      ))}
+    </Layout>
+  );
+};
